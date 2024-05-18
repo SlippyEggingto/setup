a7df963cf81955e1c4bcf9a6a4f76539515f77ca
nptanphuc <100423899+SlippyEggingto@users.noreply.github.com>
2024-05-18 15:41:49 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,52 @@
+cd ~
+mkdir ~/Downloads
+
+echo "Updating and upgrading..."
+sudo pacman -Syu
+
+echo "Installing hyprland hyprpaper waybar neovim nwg-look foot fish sway ttf-jetbrains-mono-nerd ntfs-3g..."
+sudo pacman -S hyprland hyprpaper waybar neovim nwg-look foot fish sway ttf-jetbrains-mono-nerd ntfs-3g
+
+echo "Configuring hyprpaper and waybar..."
+mv ~/setup/peakpx.jpg ~/Downloads
+mv ~/setup/wallpaper_2.jpg ~/Downloads
+mv ~/setup/hyprland.conf ~/.config/hypr
+mv ~/setup/hyprpaper.conf ~/.config/hypr
+mkdir ~/.config/waybar
+mv ~/setup/config.jsonc ~/.config/waybar
+mv ~/setup/style.css ~/.config/waybar
+
+echo "Installing Windows fonts..."
+sudo mkdir /media
+sudo mkdir /media/nptanphuc
+sudo mount /dev/sda2 /media/nptanphuc
+sudo mkdir /usr/local/share/fonts
+sudo mkdir /usr/local/share/fonts/WindowsFonts
+sudo cp /mnt/Windows/Fonts/* /usr/local/share/fonts/WindowsFonts
+fc-cache --force
+
+echo "Mounting F0C2F863C2F83008 on startup..."
+sudo cp /etc/fstab /etc/fstab.backup
+sudo echo "UUID=F0C2F863C2F83008 	/media/nptanphuc	ntfs-3g		uid=1000,gid=1000,umask=0022,sync,auto,nosuid,rw,nouser 0 0" >> /etc/fstab
+
+echo "Creating Desktop shortcut..."
+ln -s /media/nptanphuc/Users/HP/Desktop/ ~/Desktop
+
+echo "Installing nvchad..."
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+
+echo "Installing yay..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+echo "Installing ibus-bamboo google-chrome visual-studio-code-bin"
+yay -S ibus-bamboo google-chrome visual-studio-code-bin
+
+echo "Creating swap file..."
+sudo dd if=/dev/zero of=/SWAP.img bs=1M count=4096
+sudo sync
+sudo chmod 600 /SWAP.img
+sudo mkswap /SWAP.img
+sudo swapon /SWAP.img
+sudo echo "/SWAP.img none swap sw 0 0" >> /etc/fstab
