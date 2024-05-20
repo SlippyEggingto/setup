a5ae3fe990d9beda8c43aac4f3b5302f82ea56ae
nptanphuc <100423899+SlippyEggingto@users.noreply.github.com>
2024-05-20 19:28:07 +0700

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 bind \e\[1\;5C forward-word
 bind \e\[1\;5D backward-word
 
-wal -i ~/Downloads/peakpx.jpg
+wal -i ~/Downloads/wallpapers/peakpx.jpg
 
 if status is-interactive
     set fish_greeting
