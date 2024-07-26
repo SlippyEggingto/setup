c17963d1492bacabaccbd41ef744e25f9cf2cd0e
SlippyEggingto <phamtanphucnguyen@gmail.com>
2024-07-26 12:17:50 +0700

    Include hyprland-colors.sh in wallpaper.sh, to be compatible with fish

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,11 +1,11 @@
-set wallpaper /home/$USER/Downloads/wallpapers/wall_25.jpg
+set wallpaper /home/$USER/Downloads/wallpapers/wall_10.jpg
 #set waybar "waybar -s ~/.config/waybar/original.css -c ~/.config/waybar/original.jsonc"
 set waybar waybar
 
 rm ~/.config/hypr/hyprpaper.conf
 touch ~/.config/hypr/hyprpaper.conf
 echo "preload = $wallpaper
-wallpaper = eDP-1,$wallpaper" >> ~/.config/hypr/hyprpaper.conf
+wallpaper = ,$wallpaper" >> ~/.config/hypr/hyprpaper.conf
 
 rm ~/.config/swaylock/config
 touch ~/.config/swaylock/config
@@ -34,4 +34,13 @@ killall waybar
 killall hyprpaper
 hyprctl dispatch exec $waybar
 hyprctl dispatch exec hyprpaper
-bash ~/setup/hyprland-colors.sh
+
+rm /home/$USER/.cache/wal/hyprland-colors.conf
+rm /home/$USER/.cache/wal/template-colors
+touch /home/$USER/.cache/wal/hyprland-colors.conf
+touch /home/$USER/.cache/wal/template-colors
+echo "$(sed 's/#//g' /home/$USER/.cache/wal/colors)" >> /home/$USER/.cache/wal/template-colors
+
+for i in (seq 1 16)
+    echo "\$color$i = rgb($(sed "$i q;d" /home/$USER/.cache/wal/template-colors))" >> /home/$USER/.cache/wal/hyprland-colors.conf
+end
