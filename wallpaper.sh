469e28e01111bc86da555bfdbc53e787280fc862
SlippyEggingto <phamtanphucnguyen@gmail.com>
2024-08-01 21:32:02 +0700

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,4 +1,4 @@
-set wallpaper /home/$USER/Downloads/wallpapers/wall_2.jpg
+set wallpaper /home/$USER/Downloads/wallpapers/wall_29.jpg
 #set waybar "waybar -s ~/.config/waybar/original.css -c ~/.config/waybar/original.jsonc"
 set waybar waybar
 
