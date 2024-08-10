9446726c1d7eae2f01b92604428ed5ccb9a593bf
SlippyEggingto <phamtanphucnguyen@gmail.com>
2024-08-10 18:05:53 +0700

    Add new wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,4 +1,4 @@
-set wallpaper "/home/$USER/Downloads/wallpapers/wall_29.jpg"
+set wallpaper "/home/$USER/Downloads/wallpapers/wall_32.png"
 #set waybar "waybar -s ~/.config/waybar/original.css -c ~/.config/waybar/original.jsonc"
 set waybar waybar
 
