bcde88b1a2783bc291c66f1db4519c3ec45202be
SlippyEggingto <phamtanphucnguyen@gmail.com>
2024-08-11 13:15:14 +0700

    Add mako/, update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,6 @@
-set wallpaper "/home/$USER/Downloads/wallpapers/wall_32.png"
+wallpaper="/home/$USER/Downloads/wallpapers/wall_32.png"
 #set waybar "waybar -s ~/.config/waybar/original.css -c ~/.config/waybar/original.jsonc"
-set waybar waybar
+waybar=waybar
 
 rm ~/.config/hypr/hyprpaper.conf
 touch ~/.config/hypr/hyprpaper.conf
@@ -10,15 +10,20 @@ wallpaper = ,\$wallpaper" >> ~/.config/hypr/hyprpaper.conf
 
 wal -i $wallpaper -q
 
+sed -i -E "s/#.*?/$(sed '5q;d' ~/.cache/wal/colors)/" ~/setup/mako/config
+makoctl reload
+notify-send "Changing colorscheme and wallpaper"
+
 rm /home/$USER/.cache/wal/hyprland-colors.conf
 rm /home/$USER/.cache/wal/template-colors
 touch /home/$USER/.cache/wal/hyprland-colors.conf
 touch /home/$USER/.cache/wal/template-colors
 echo "$(sed 's/#//g' /home/$USER/.cache/wal/colors)" >> /home/$USER/.cache/wal/template-colors
 
-for i in (seq 1 16)
+for i in $(seq 1 16)
+do
     echo "\$color$i = rgb($(sed "$i q;d" /home/$USER/.cache/wal/template-colors))" >> /home/$USER/.cache/wal/hyprland-colors.conf
-end
+done
 
 rm ~/.cache/wal/colors-oomox
 touch ~/.cache/wal/colors-oomox
@@ -46,3 +51,5 @@ killall waybar
 killall hyprpaper
 hyprctl dispatch exec $waybar
 hyprctl dispatch exec hyprpaper
+
+notify-send "Colorscheme and wallpaper changed" "Wallpaper: $wallpaper\n"
