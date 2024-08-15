49d6bd7a1bafd51293205632ad1a880ea30cde5d
SlippyEggingto <phamtanphucnguyen@gmail.com>
2024-08-15 13:24:35 +0700

    Update eww/

--- eww/scripts/notification.sh
+++ eww/scripts/notification.sh
@@ -10,7 +10,10 @@ notification() {
         body="$(makoctl history | jq .data.[0].[$i].body.data)"
         temp+="(button :class \"notification-cell\" (box :class \"notification-cell-content\" :space-evenly false :spacing 20 (image :class \"notification-cell-image\" :path \"./icons/icon_3.jpg\" :image-width 30 :image-height 30) (box :class \"notification-cell-label\" :orientation \"vertical\" :space-evenly false :valign \"center\" :spacing 5 (label :class \"notification-cell-label-summary\" :wrap true :xalign 0 :yalign 1 :halign \"start\" :text $summary :show-truncated false :limit-width 100) (label :class \"notification-cell-label-body\" :wrap true :xalign 0 :yalign 1 :halign \"start\" :text $body :show-truncated false :limit-width 100))))"
     done
-    echo "(scroll :class \"notification-scroll\" :orientation \"vertical\" :height 300 (box :class \"notification\" :orientation \"vertical\" :spacing 10 :height 600 $temp))"
+    echo "(scroll :class \"notification-scroll\" :orientation \"vertical\" :height 350 (box :class \"notification\" :orientation \"vertical\" :spacing 10 :height 600 $temp))"
 }
 
-notification
+# dbus-monitor "interface='org.freedesktop.Notifications'" | grep --line-buffered "member=Notify\|string" |
+# while read -r line; do
+    notification
+# done
